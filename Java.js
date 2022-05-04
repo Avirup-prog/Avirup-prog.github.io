8424117e23f6228149ca41e512ac53d1e5938fdc
Avirup-prog <104582739+Avirup-prog@users.noreply.github.com>
2022-05-04 19:49:38 +0530

    New

--- Java.js
+++ Java.js
@@ -305,3 +305,7 @@ inputs.forEach((input) => {
 	input.addEventListener("focus", focusFunc);
 	input.addEventListener("blur", blurFunc);
 });
+const trigger = document.querySelector("menu > .trigger");
+trigger.addEventListener('click', (e) => {
+	e.currentTarget.parentElement.classList.toggle("open");
+});
